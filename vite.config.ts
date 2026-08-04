b820e38973b4e5a88a7e0d32d3a9b8738244ced3
mm <25961416+mlmoravek@users.noreply.github.com>
2026-08-04 15:25:02 +0200

    fix(build): correct scss copy in build process (#10)

--- vite.config.ts
+++ vite.config.ts
@@ -50,7 +50,13 @@ export default defineConfig(({ mode }) => {
                 }),
                 // copy assets into dist
                 copy({
-                    targets: [{ src: "src/assets/scss", dest: "." }],
+                    targets: [
+                        {
+                            src: "src/assets/scss",
+                            dest: ".",
+                            rename: { stripBase: 2 },
+                        },
+                    ],
                 }),
                 // adds a banner to every generated dist file
                 banner(generate(pkg.version)),
