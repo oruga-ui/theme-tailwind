56ae096fd447da0d063c334a51a76b8d87c695d2
Mouad Taoussi <50294614+mouadTaoussi@users.noreply.github.com>
2026-09-23 01:02:22 +0200

    Fix active in menu

--- src/plugins/theme.ts
+++ src/plugins/theme.ts
@@ -461,7 +461,7 @@ const tailwindConfig: OrugaConfig = {
         listClass: "menu-list",
         itemButtonClass:
             "menu-item text-left text-gray-700 dark:text-gray-300 w-full rounded [&_.icon]:mr-2 [&_.icon]:ml-1",
-        itemButtonActiveClass: "active",
+        itemButtonActiveClass: "active bg-blue-700 text-white",
         itemButtonDisabledClass:
             "disabled pointer-events-none cursor-not-allowed opacity-50",
         itemSubmenuClass: "submenu ml-5 pl-1 border-l border-gray-200 dark:border-gray-600",
